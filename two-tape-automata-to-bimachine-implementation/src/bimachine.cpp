621494c4010e64ed64306ada0c02a34165e4c26b
meglenast <58610466+meglenast@users.noreply.github.com>
2021-07-06 21:59:41 +0300

    Bimachine's left states

--- two-tape-automata-to-bimachine-implementation/src/bimachine.cpp
+++ two-tape-automata-to-bimachine-implementation/src/bimachine.cpp
@@ -1,6 +1,7 @@
 #include "bimachine.h"
 
-Bimachine::Bimachine(const TwoTapeAutomata& T)
+Bimachine::Bimachine(const TwoTapeAutomata& T):
+	T(&T)
 {
 	generateRightAutomata(T);
 	generateLeftAutomata (T);
@@ -14,11 +15,10 @@ void Bimachine::generateRightAutomata(const TwoTapeAutomata& T)
 	std::map<std::pair<size_t, size_t>, std::vector<std::pair<char, std::pair<size_t, size_t>>>> reverseDeltaT;
 	generateDeltaRev(T, reverseDeltaT);
 
-	std::vector < std::set<std::pair<size_t, size_t>>> statesR;
-	std::map < std::set<std::pair<size_t, size_t>>, size_t> statesR_RemapHashValues;
-
+	//std::vector < std::set<std::pair<size_t, size_t>>> statesR;
+	
 	statesR.push_back(T.getFinalStates());
-	statesR_RemapHashValues.insert({ T.getFinalStates(), 0 });
+	statesR_mappedValues.insert({ T.getFinalStates(), 0 });
 
 	size_t cntStates = 1;
 
@@ -29,26 +29,27 @@ void Bimachine::generateRightAutomata(const TwoTapeAutomata& T)
 
 		generateReachedStatesFrom(currState, currTrans, reverseDeltaT);
 
-		generateStatesRightAutomataFromCurrTrans(currTrans, statesR, statesR_RemapHashValues, cntStates);
+		generateStatesRightAutomataFromCurrTrans(currTrans, statesR, statesR_mappedValues, cntStates);
 		
-		generateTransitionsRightAtomataFrom(i, currTrans, statesR_RemapHashValues);
+		generateTransitionsRightAtomataFrom(i, currTrans, statesR_mappedValues);
 	}
 
-	for (size_t i = 0; i < statesR.size(); i++)
-		statesRight.push_back(i);
+	/*for (size_t i = 0; i < statesR.size(); i++)
+		statesRight.push_back(i);*/
 }
 
 void Bimachine::generateLeftAutomata(const TwoTapeAutomata& T)
 {
-	
-	std::map<size_t, std::set<std::pair<size_t, char>>> deltaR_helper;
-	reverseDeltaRight(deltaR_helper);
-
 	std::vector<TwoTapeTransition> deltaT = T.getTransitions();
-
+	std::map<size_t, std::set<std::pair<size_t, char>>> deltaR_helper;
 	std::map<char, std::map<std::pair<size_t,size_t>, std::set<std::pair<std::pair<size_t,size_t>, char>>>> deltaT_helper;
-    
-	mapCharToTransitionAndOutputInTransducer(deltaT_helper, deltaT);	
+	std::map<size_t, std::pair<size_t, size_t>> selector_func;
+
+	reverseDeltaRight(deltaR_helper);
+	mapCharToTransitionAndOutputInTransducer(deltaT_helper, deltaT);
+	initSelector();
+	generateLeftStates(deltaT_helper, deltaR_helper);
+
 }
 
 void Bimachine::generateDeltaRev(const TwoTapeAutomata& T, std::map<std::pair<size_t, size_t>, std::vector<std::pair<char, std::pair<size_t, size_t>>>>& reverseDeltaT)
@@ -143,7 +144,6 @@ void Bimachine::reverseDeltaRight(std::map<size_t, std::set<std::pair<size_t, ch
 	}
 }
 
-
 //remap deltaT 
 // <char> --> ( <q_beg> --> {<q_end, str>} ) for every transition in T
 void Bimachine::mapCharToTransitionAndOutputInTransducer(std::map<char, std::map<std::pair<size_t, size_t>, std::set<std::pair<std::pair<size_t, size_t>, char>>>>& deltaT_helper, std::vector<TwoTapeTransition>& deltaT)
@@ -169,6 +169,91 @@ void Bimachine::mapCharToTransitionAndOutputInTransducer(std::map<char, std::map
 			deltaT_helper.at(fst_tape_letter).insert({ from,curr });
 		}
 		deltaT_helper.at(fst_tape_letter).at(from).insert({ to, scnd_tape_letter });
+	}
+}
+
+void Bimachine::initSelector()
+{
+	for (std::vector<std::set<std::pair<size_t, size_t>>>::iterator it_state = statesR.begin(); it_state != statesR.end(); it_state++)
+	{
+		std::pair<size_t, size_t>  initT = T->getInitialState();
+
+		if ((*it_state).find(initT) != (*it_state).end())
+		{
+			selector_func.insert({ statesR_mappedValues.at((*it_state)), initT });
+			break;
+		}
+	}
+}
+
+void Bimachine::generateLeftStates(std::map<char, std::map<std::pair<size_t, size_t>, std::set<std::pair<std::pair<size_t, size_t>, char>>>>& deltaT_helper, std::map<size_t, std::set<std::pair<size_t, char>>>& deltaR_helper)
+{
+	std::map<char, std::map<size_t,size_t>> new_selectorFunc;
+	size_t cntStates = 0;
+
+	statesL.push_back({ {T->getInitialState()}, selector_func });
+	statesL_mappedValues.insert({ { {T->getInitialState()}, selector_func }, 0});
+
+	cntStates += 1;
+
+
+	for (size_t it_statesL = 0; it_statesL < statesL.size(); it_statesL++)
+	{
+		std::set<std::pair<size_t,size_t>> curr_state        = statesL[it_statesL].first;
+		selectorFunc_p                     curr_selectorFunc = statesL[it_statesL].second;
+		
+
+		//calculate leftTransitionsFrom and selector function
+
+		std::map<char, leftState_p>             curr_left_transitions;
+		std::map<char, std::map<size_t, size_t>> curr_select_func;
+
+		for (size_t letter = 0; letter < SIGMA.size(); letter++)
+		{
+			if (deltaT_helper.find(SIGMA[letter]) != deltaT_helper.end())
+			{
+				std::set<std::pair<size_t,size_t>> newLeft;
+
+				for (selectorFunc_p::iterator it_selector = curr_selectorFunc.begin(); it_selector != curr_selectorFunc.end(); it_selector++)
+				{
+					if (deltaR_helper.find((*it_selector).first) != deltaR_helper.end())
+					{
+						std::set<std::pair<size_t, char>> curr_trans = deltaR_helper[(*it_selector).first];
+
+						for (std::set<std::pair<size_t, char>>::iterator it_transitions = curr_trans.begin(); it_transitions != curr_trans.end(); it_transitions++)
+						{
+							if (SIGMA[letter] == it_transitions->second) // transiton dest
+							{
+								for (auto& to : deltaT_helper.at(SIGMA[letter]).at(it_selector->second))
+								{
+									if (statesR[it_transitions->first - 1].find(to.first) != statesR[it_transitions->first-1].end())
+									{
+										if (new_selectorFunc.find(SIGMA[letter]) == new_selectorFunc.end())
+										{
+											std::map<size_t, size_t> curr;
+											new_selectorFunc.insert({ SIGMA[letter], curr });
+										}
+										new_selectorFunc.at(SIGMA[letter]).insert({ it_transitions->first, to.second });
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+				for (auto& state : deltaT_helper.at(SIGMA[letter]))
+				{
+					if (curr_state.find(state.first) != curr_state.end())
+					{
+						for (auto& p : state.second)
+						{
+							newLeft.insert(p.first);
+						}
+					}
+				}
+			}
+			
+		}
 	}
 }
 
--- two-tape-automata-to-bimachine-implementation/src/bimachine.h
+++ two-tape-automata-to-bimachine-implementation/src/bimachine.h
@@ -4,6 +4,9 @@
 #include <unordered_map>
 #include "twoTapeAutomata.h"
 
+typedef std::map<size_t, std::pair<size_t, size_t>> selectorFunc_p;
+typedef std::pair<std::set<std::pair<size_t,size_t>>, selectorFunc_p> leftState_p;
+
 struct bimachineTransition
 {
 	size_t state_left;
@@ -30,13 +33,22 @@ private:
 	/*
 	DFA* left_automata;
 	DFA* right_automata;*/
+	const TwoTapeAutomata* T;
 
-	//std::set<size_t> statesRight;
-	size_t statesRight;
+
+	std::map < std::set<std::pair<size_t, size_t>>, size_t> statesR_mappedValues;
+	std::map < leftState_p, size_t> statesL_mappedValues;
+
+
+	std::vector<std::set<std::pair<size_t, size_t>>> statesR;
+	// 
+	std::vector<leftState_p> statesL;
 	size_t initStateRight;
 	std::set<std::pair<std::pair<size_t, char>, size_t>> transitionsRight;
 
 	std::vector<bimachineTransition> outputFunciton;
+
+	std::map<size_t, std::pair<size_t, size_t>> selector_func;
 
 
 private:
@@ -61,6 +73,10 @@ private:
 	void reverseDeltaRight(std::map<size_t, std::set<std::pair<size_t, char>>>&);
 
 	void mapCharToTransitionAndOutputInTransducer(std::map<char, std::map<std::pair<size_t, size_t>, std::set<std::pair<std::pair<size_t, size_t>, char>>>>& deltaT_helper, std::vector<TwoTapeTransition>& deltaT);
+
+	void initSelector();
+
+	void generateLeftStates(std::map<char, std::map<std::pair<size_t, size_t>, std::set<std::pair<std::pair<size_t, size_t>, char>>>>&, std::map<size_t, std::set<std::pair<size_t, char>>>&);
 
 	std::vector < std::set<std::pair<size_t, size_t>>>::iterator findExist(std::vector < std::set<std::pair<size_t, size_t>>>&, std::set<std::pair<size_t, size_t>>);
 };
